647e26bf4eadf20780cd919bbc559fb47561d679
Ilya Spivak <st.remedy@icloud.com>
2025-05-13 21:24:42 +0500

    Add TripleInputTextButHarder

--- Lesson-1/TripleInputTextButHarder.java
+++ Lesson-1/TripleInputTextButHarder.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
 
-
-class TripleInputText {
-
-    public static void main(String[] args) {
+class TripleInputTextButHarder {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        System.out.println(text);
+        System.out.println(text + " " + text);
         System.out.println(text);
         System.out.println(text);
     }

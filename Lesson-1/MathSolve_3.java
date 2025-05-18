62beec18bdbdb168cf65d495d8a15efca5e5659e
Ilya Spivak <st.remedy@icloud.com>
2025-05-18 11:30:23 +0500

    MathSolve_3

--- Lesson-1/MathSolve_3.java
+++ Lesson-1/MathSolve_3.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class MathSolve_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int squareX = (int) Math.pow(num, 2);
+        int cubeX = (int) Math.pow(num, 3);
+        System.out.print(num + " ");
+        System.out.print(squareX + " ");
+        System.out.print(cubeX);
+    }
 }
